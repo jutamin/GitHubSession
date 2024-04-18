4bb436bbe2c7ffbbf06356cd7f0d14253813b45f
jutamin <wonjy0307@gmail.com>
2024-04-18 20:17:25 +0900

    Initial Commit

---
+++ Gitit/GititApp.swift
@@ -0,0 +1,17 @@
+//
+//  GititApp.swift
+//  Gitit
+//
+//  Created by 원주연 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GititApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
